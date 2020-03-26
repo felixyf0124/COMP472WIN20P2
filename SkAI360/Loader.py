dfe70fb1f84f4ed5b0968bbeb5cc452247c1df48
Kevin L <Lin_Kevin_1995@hotmail.com>
2020-03-25 22:16:12 -0400

    #4 Added Comments and file encoding parameter for parsing errors.

--- SkAI360/Loader.py
+++ SkAI360/Loader.py
@@ -1,10 +1,17 @@
 class Loader:
+    # vocabularyLevel 0: Fold the corpus to lowercase and use only the 26 letters of the alphabet [a-z]
+    # vocabularyLevel 1: Distinguish up and low cases and use only the 26 letters of the alphabet [a-z, A-Z]
+    # vocabularyLevel 2: Distinguish up and low cases and use all characters accepted by the built-in isalpha() method
+    # nGramSize: 1 = character unigrams, 2 = character bigrams, 3 = character trigrams
+    # smoothingValue: real number representing the smoothing value
+    # trainingPath: training path file
+    # testingPath: testing path file
     def __init__(self, vocabularyLevel, nGramSize, smoothingValue, trainingPath, testingPath):
         print('vocabularyLevel: ', vocabularyLevel)
         print('nGramSize: ', nGramSize)
         print('smoothingValue: ', smoothingValue)
 
-        trainingFile = open(trainingPath, "r")
+        trainingFile = open(trainingPath, "r", encoding="utf8")
         lineNum = 0
         for line in trainingFile:
             lineNum += 1
@@ -12,7 +19,7 @@ class Loader:
         print(lineNum)
         trainingFile.close()
 
-        testingFile = open(testingPath, "r")
+        testingFile = open(testingPath, "r", encoding="utf8")
         lineNum = 0
         for line in testingFile:
             lineNum += 1
