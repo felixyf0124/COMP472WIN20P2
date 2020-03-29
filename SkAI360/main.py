538066afc8fcf162599592018d47458ee916fd01
felixyf0124 <felixyf0124@gmail.com>
2020-03-28 23:06:37 -0400

    #1 naming convention

--- SkAI360/main.py
+++ SkAI360/main.py
@@ -1,6 +1,6 @@
-import Loader as ld
+from loader import Loader as ld
 
 trainingFile = "dataset/training-tweets.txt"
 testingFile = "dataset/test-tweets-given.txt"
 
-loader = ld.Loader(0, 1, 0.1, trainingFile, testingFile)
+loader = ld(0, 1, 0.1, trainingFile, testingFile)
