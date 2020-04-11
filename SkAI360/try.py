5fcbaa91065599565e70bd90a74fcf047046164d
felixyf0124 <felixyf0124@gmail.com>
2020-04-11 16:26:36 -0400

    check language distributions for different data set
    Co-Authored-By: Kevin Lin <lin_kevin_1995@hotmail.com>

--- SkAI360/try.py
+++ SkAI360/try.py
@@ -22,30 +22,65 @@ demoParam.append([2, 3, 0.01])
 demoParam.append([2, 3, 0.001])
 
 # main execute loop
-for param in demoParam:
-    for e in extra:
-        print('vocabulary =', param[0], 'ngram =', param[1],
-              'delta =', param[2], 'extra =', e)
-        loader = ld(verbose)
-        trainer = Trainer(param[0], param[1], param[2], verbose, e)
-        loader.resetLineCursor()
+# for param in demoParam:
+#     for e in extra:
+#         print('vocabulary =', param[0], 'ngram =', param[1],
+#               'delta =', param[2], 'extra =', e)
+#         loader = ld(verbose)
+#         trainer = Trainer(param[0], param[1], param[2], verbose, e)
+#         loader.resetLineCursor()
 
-        line = loader.getNextLineInTrainingData()
+#         line = loader.getNextLineInTrainingData()
 
-        while (line != None):
-            trainer.feedLineInfo(line[2], line[3])
-            line = loader.getNextLineInTrainingData()
+#         while (line != None):
+#             trainer.feedLineInfo(line[2], line[3])
+#             line = loader.getNextLineInTrainingData()
 
-        # trainer.fillRestTable()
+#         # trainer.fillRestTable()
 
-        tester = Tester(trainer)
-        loader.resetLineCursor()
-        line = loader.getNextLineInTestingData()
+#         tester = Tester(trainer)
+#         loader.resetLineCursor()
+#         line = loader.getNextLineInTestingData()
 
-        while(line != None):
-            tester.doTestLine(line, loader.getLineCursorPos())
-            line = loader.getNextLineInTestingData()
+#         while(line != None):
+#             tester.doTestLine(line, loader.getLineCursorPos())
+#             line = loader.getNextLineInTestingData()
 
-        print(tester.getAccuracy())
+#         print(tester.getAccuracy())
 
-        tester.dumpResult()
+#         tester.dumpResult()
+
+tweets = dict()
+tweets["eu"] = 0
+tweets["ca"] = 0
+tweets["gl"] = 0
+tweets["es"] = 0
+tweets["en"] = 0
+tweets["pt"] = 0
+
+loader = ld(verbose)
+loader.resetLineCursor()
+line = loader.getNextLineInTrainingData()
+while (line != None):
+    for key in tweets:
+        if(key == line[2]):
+            tweets[key] += 1
+
+    line = loader.getNextLineInTrainingData()
+
+total = 0
+
+for key in tweets:
+    total += tweets[key]
+    # print(key + " " + str(tweets[key]))
+
+trainSR = "Training Set Ratio"
+testSR1 = "Initial Testing Set Ratio"
+testSR2 = "Demo Testing Set Ratio"
+print(testSR2)
+for key in tweets:
+    # total += tweets[key]
+    print("   " + key + " " + "{:6d}".format(tweets[key]) +
+          " " + "{:3.2f}".format(tweets[key]/total*100) + " %")
+
+print("total " + "{:6d}".format(total))
